e0324eccfe693ce1be544743ff3fe85fafbf26a1
Trust Birungi <trust.birungi@andela.com>
2018-11-06 14:43:56 +0300

    add lists of articles

--- src/App.js
+++ src/App.js
@@ -4,22 +4,52 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const siteName = "HackerNews";
+    const tagLine = "We'll stop writing it when you stop reading it!";
+    const helloWorld = "Welcome To HackerNews!";
+
+    let user = {
+      firstName: 'Trust',
+      lastName: 'Birungi',
+    };
+
+    const list = [
+      {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+      },
+      {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+      },
+    ];
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>{siteName}</h1>
+        <h1>||</h1>
+        <h2>{tagLine}</h2>
+        <h2>{user.firstName + ' ' + user.lastName}, {helloWorld}</h2>
+        <br /><br />
+
+        {list.map(item =>
+            <div key={item.objectID}>
+              <span>
+              <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{' ' + item.author}</span>
+              <span>{' ' + item.num_comments}</span>
+              <span>{' ' + item.points}</span>
+            </div>
+        )}
       </div>
     );
   }
